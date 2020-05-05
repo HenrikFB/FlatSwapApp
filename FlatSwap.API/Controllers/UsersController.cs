b620436a61742fe4c2202de1aa70c4cfcc2e51fa
HenrikFB <henrik_1993@hotmail.com>
2020-05-05 17:40:27 +0200

    Action Filters

--- FlatSwap.API/Controllers/UsersController.cs
+++ FlatSwap.API/Controllers/UsersController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using AutoMapper;
 using FlatSwap.API.Data;
 using FlatSwap.API.Dtos;
+using FlatSwap.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlatSwap.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
